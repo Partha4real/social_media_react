1c6e4b9f1197114055594c89965d19c0718e2928
Partha4real <67099030+Partha4real@users.noreply.github.com>
2021-01-05 12:22:34 +0530

    UI for post list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      App 3
-    </div>
-  );
+import {fetchPosts} from '../actions/posts';
+import {PostsList} from './'
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  
+
+  render() {
+    //console.log('Props', this.props);
+    const {posts} = this.props;
+    return (
+      <div>
+        <PostsList posts= {posts}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps (state) {   // we are basicall mapping all our redux state to our store
+  return {
+    posts: state.posts
+  }
+}
+export default connect(mapStateToProps)(App);
+ 
