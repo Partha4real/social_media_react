5ebc042a1cbdd57004c5d8909730d96ee044ee91
Partha4real <67099030+Partha4real@users.noreply.github.com>
2021-01-07 23:11:08 +0530

    Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -19,7 +19,7 @@ class Chat extends Component {
     }
   }
 
-  setupConnections = () => {
+  setupConnections = () => {  // in progress
     const socketConnection = this.socket;
     const self = this;
 
